b2a0605eb6e72712310fbe1a481cc98083ca965b
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-02 12:52:28 +0530

    [Rishi]Refactor:parttimewage

--- Employewage/Employewage/EmployeWage.cs
+++ Employewage/Employewage/EmployeWage.cs
@@ -9,27 +9,30 @@ namespace Employewage
 
         public void EmpPresent()
         {
-            const int IS_PRESENT = 1;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
             const int EMP_WAGE_PER_HR = 20;
 
-            //Variables
             int employeeHr = 0;
             int totalEmployeeWage = 0;
             Random objRandom = new Random();
-
-            //Genarating random number for check employee is present or not
-            int Check = objRandom.Next(0, 2);
-            if (Check == IS_PRESENT)
+            int Check = objRandom.Next(0, 3);
+            if (Check == IS_FULL_TIME)
             {
                 employeeHr = 8;
-                Console.WriteLine(" Employee Is Present");
+                Console.WriteLine(" Employee Is Full time Employee");
+            }
+            else if (Check == IS_PART_TIME)
+            {
+                employeeHr = 4;
+                Console.WriteLine(" Employee is Part time Employee");
             }
             else
             {
                 employeeHr = 0;
-                Console.WriteLine(" Employee is absent");
+                Console.WriteLine("Employee Is Absent");
             }
-            //calculating Employee wage
+
             totalEmployeeWage = employeeHr * EMP_WAGE_PER_HR;
             Console.WriteLine("Employee Wage is :" + totalEmployeeWage);
         }
