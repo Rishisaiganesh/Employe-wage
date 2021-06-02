eac3b6e6951db60350275f3678f928fe59113508
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-02 12:42:44 +0530

    [Rishi]Refactor:Attendance

---
+++ Employewage/Employewage/EmployeWage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employewage
+{
+    class EmployeWage
+    {
+
+        public void EmpPresent()
+        {
+            const int IS_PRESENT = 1;
+            Random objRandom = new Random();
+            int Check = objRandom.Next(0, 2);
+            if (Check == IS_PRESENT)
+            {
+                Console.WriteLine(" Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine(" Employee is absent");
+            }
+        }
+    }
+}
---
+++ Employewage/Employewage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Employewage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeWage emp = new EmployeWage();
+            emp.EmpPresent();
+        }
+    }
+}
