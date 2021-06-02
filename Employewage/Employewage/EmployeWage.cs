45ff5924625efca0d8c5b40be67f0eaabd57b8fe
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-02 13:04:47 +0530

    [Rishi]Refactor:MonthlyWage

--- Employewage/Employewage/EmployeWage.cs
+++ Employewage/Employewage/EmployeWage.cs
@@ -12,31 +12,37 @@ namespace Employewage
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
             const int EMP_WAGE_PER_HR = 20;
+            const int MAX_WORKING_DAYS = 20;
 
             int employeeHr = 0;
             int totalEmployeeWage = 0;
-            Random objRandom = new Random();
-            int Check = objRandom.Next(0, 3);
-            switch (Check)
+            int MonthlyWage = 0;
+            for (int i = 0; i < MAX_WORKING_DAYS; i++)
             {
-                case IS_FULL_TIME:
-                    employeeHr = 8;
-                    Console.WriteLine("Employee is full Time");
-                    break;
-                case IS_PART_TIME:
-                    Console.WriteLine("Emloyee is part time");
-                    employeeHr = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    employeeHr = 0;
-                    break;
+                Random objRandom = new Random();
+                int Check = objRandom.Next(0, 3);
+                switch (Check)
+                {
+                    case IS_FULL_TIME:
+                        employeeHr = 8;
+                        Console.WriteLine("Employee is full Time");
+                        break;
+                    case IS_PART_TIME:
+                        Console.WriteLine("Emloyee is part time");
+                        employeeHr = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        employeeHr = 0;
+                        break;
+                }
+
+
+                //calculating Employee wage
+                totalEmployeeWage = employeeHr * EMP_WAGE_PER_HR;
+                MonthlyWage = MonthlyWage + totalEmployeeWage;
+                Console.WriteLine("Employee Wage is :" + MonthlyWage);
             }
-
-
-            //calculating Employee wage
-            totalEmployeeWage = employeeHr * EMP_WAGE_PER_HR;
-            Console.WriteLine("Employee Wage is :" + totalEmployeeWage);
         }
     }
 }
